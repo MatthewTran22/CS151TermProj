8cccda4b1ab7ca1a1275ea8c1f77d61da5cd92ef
Aditya Rao <aditya.k.rao@sjsu.edu>
2024-10-17 13:11:36 -0700

    Create AccountList.java

---
+++ Application/AccountList.java
@@ -0,0 +1,36 @@
+package application;
+import java.util.*; 
+import java.lang.*; 
+
+// random id genration
+import java.util.UUID; 
+
+public class AccountList {
+	private ArrayList<Account> accountList = new ArrayList<>(); // change when we add the db
+	
+	public void addAccount(String bankName, double balance, String date) {
+		UUID rawID = UUID.randomUUID();
+		String id = rawID.toString(); 
+		
+		accountList.add(new Account(id, bankName, balance, date)); // change when we add the db
+	}
+	
+	public boolean deleteAccount(String id) {	
+		for(int i=0; i<accountList.size(); i++) { // change when we add the db
+			Account accountTemp = accountList.get(i); 
+			
+			if(accountTemp.getId() == id) {
+				accountList.remove(i);
+				return true; 
+			}
+		}
+		
+		return false;
+	}
+	
+	public ArrayList<Account> getList(ArrayList<Account> accountList) { // return accounts list
+	    return new ArrayList<>(accountList);
+	}
+		
+}
+
