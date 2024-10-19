542f84f1429369b2ffae0543e158f76cb018b588
Matthew Tran <matthewtran0122@gmail.com>
2024-10-19 12:51:30 -0700

    Added input field verification

--- src/application/controller/newAccController.java
+++ src/application/controller/newAccController.java
@@ -1,10 +1,38 @@
 package application.controller;
 
+
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class newAccController {
 
+	@FXML DatePicker dateInput;
+	@FXML TextField nameInput;
+	@FXML TextField balanceInput;
+	@FXML Label msgLabel;
+	
+	String name;
+	double openBalance;
+	String openDate;
+	
+
 	@FXML public void handleSubmitOP() {
+		msgLabel.setText("");
+
+		try {
+			name = nameInput.getText();
+			openBalance = Double.parseDouble(balanceInput.getText());
+			openDate = dateInput.getAccessibleText();
+			msgLabel.setText("Account added");
+			
+		}
+		catch (Exception e) {
+			msgLabel.setText("Invalid input");
+			
+		}
+		
 		
 		
 	}
